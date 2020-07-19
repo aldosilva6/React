9b62179505f278034ad8a4c9daeec756a38d73c1
Aldo Inácio da Silva <aldosilva6@gmail.com>
2020-07-19 17:06:46 -0300

    Added components to learn reduce

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,31 +1,36 @@
-import React from "react";
+import React, { useReducer } from "react";
 import "./App.css";
-import ComponentC from "./components/ComponentC";
-import HookCounterReducer from "./components/HookCounterReducer";
-import HookCounterTwoReducer from "./components/CounterTwoReducer";
+import ComponentA from "./components/ComponentA";
+import ComponentD from "./components/ComponentD";
+import ComponentG from "./components/ComponentG";
 
-export const UserContext = React.createContext();
-export const ChannelContext = React.createContext();
+export const CountContext = React.createContext();
+
+const initialState = 0;
+const reducer = (state, action) => {
+  switch (action) {
+    case "increment":
+      return state + 1;
+    case "decrement":
+      return state - 1;
+    case "reset":
+      return initialState;
+    default:
+      return state;
+  }
+};
 
 function App() {
+  const[count, dispatch] = useReducer(reducer, initialState)
   return (
+    <CountContext.Provider value={{ countState: count, countDispatch: dispatch }}>
     <div className="App">
-      {/*<HookCounterFour />*/}
-      {/*<ClassCounterTwo />*/}
-      {/*<ClassCounter />*/}
-      {/*<HookCounterTwo />*/}
-      {/*<HookCounterThree />*/}
-      {/* <DataFeching /> */}
-      {/* <MouseContainer /> */}
-      {/* <ChannelContext.Provider value={"Aldo"}>
-        <UserContext.Provider value={"Vishwas"}>
-          <ComponentC />
-        </UserContext.Provider>
-      </ChannelContext.Provider> */}
-
-      {/* <HookCounterReducer /> */}
-      <HookCounterTwoReducer />
+      Count - {count}
+      <ComponentA />
+      <ComponentD />
+      <ComponentG />
     </div>
+    </CountContext.Provider>
   );
 }
 
--- my-app/src/components/HookCounterTwoReducer.js
+++ my-app/src/components/HookCounterTwoReducer.js
@@ -2,15 +2,20 @@ import React, { useReducer } from "react";
 
 const initialState = {
   firstCounter: 0,
+  secondCounter: 10
 };
 
 //Using state as object can keep track of two different counters
 const reducer = (state, action) => {
   switch (action.type) {
     case "increment":
-      return { firstCounter: state.firstCounter + action.value };
+      return { ...state, firstCounter: state.firstCounter + action.value };
     case "decrement":
-      return { firstCounter: state.firstCounter - action.value };
+      return { ...state, firstCounter: state.firstCounter - action.value };
+    case "increment2":
+      return { ...state, secondCounter: state.secondCounter + action.value };
+    case "decrement2":
+      return { ...state, secondCounter: state.secondCounter - action.value };
     case "reset":
       return initialState;
     default:
@@ -23,7 +28,8 @@ function HookCounterTwoReducer() {
 
   return (
     <div>
-      <div>Count - {count.firstCounter}</div>
+      <div>First Counter - {count.firstCounter}</div>
+      <div>Seconde Counter - {count.secondCounter}</div>
       {/* dispatch pass the argument corresponding to the action */}
       <button onClick={() => dispatch({ type: "increment", value: 1 })}>
         Increment
@@ -37,6 +43,14 @@ function HookCounterTwoReducer() {
       <button onClick={() => dispatch({ type: "decrement", value: 5 })}>
         Decrement 5
       </button>
+      <div>
+        <button onClick={() => dispatch({ type: "increment2", value: 1 })}>
+          Increment Counter 2
+      </button>
+        <button onClick={() => dispatch({ type: "decrement2", value: 1 })}>
+          Decrement Counter 2
+      </button>
+      </div>
       <button onClick={() => dispatch({ type: "reset" })}>Reset</button>
     </div>
   );
